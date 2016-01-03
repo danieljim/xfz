8c3669d9ba4083464b16c6c3d1fd90023ebd0a8b
dojay <wdojay@gmail.com>
2016-01-03 21:41:45 +0800

    chufa

--- src/js/list.js
+++ src/js/list.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 		                '<ul class="list-2">'+
 		                    '<li>'+
 		                        '<div class="photo">'+
-		                            '<img src="http://img2.3lian.com/2014/f5/158/d/87.jpg">'+
+		                            '<img src="{{data.simgUrl}}">'+
 		                        '</div>'+
 		                        '<div class="piao-count">100</div>'+
 		                        '<div class="name">王东杰</div>'+
@@ -40,34 +40,69 @@ $(document).ready(function() {
 
 		getList : function() {
 			var _this = this;
-			$.ajax({
-		        url : '/home/banner',
-		        type : 'get',
-		        dataType : 'json',
-		        success : function(msg) {
-		            if (msg.code != '0000') {
-		                return;
-		            }
-		            var first = template.compile(tpl.firstPage),
-                	html = render({data : msg.data[0]});
+			// $.ajax({
+		 //        url : '/home/banner',
+		 //        type : 'get',
+		 //        dataType : 'json',
+		 //        success : function(msg) {
+		 //            if (msg.code != '0000') {
+		 //                return;
+		 //            }
+		 //            var first = template.compile(tpl.firstPage),
+   //              	html = render({data : msg.data[0]});
 
-                	var other = template.compile(tpl.otherPage),
-                	html = render({data : msg.data[0]});
-		            $('.head-time').append(html);
+   //              	var other = template.compile(tpl.otherPage),
+   //              	html = render({data : msg.data[0]});
+		 //            $('.head-time').append(html);
 
-		            slide.init();
-		        }
-		    });
+		 //            _this.bind();
+		 //        }
+		 //    });
+
+			var data = {
+						    "code": "0000",
+						    "msg": "处理成功",
+						    "data": [
+						        {
+						            "id": 16,
+						            "mainTitle": "",
+						            "subTitle": "",
+						            "simgUrl": "http://spimg.qunarzz.com/gongyu/20150806/qunarbanner_150X60.jpg",
+						            "imgUrl": "",
+						            "bimgUrl": "http://spimg.qunarzz.com/gongyu/20150806/qunarbanner_2000x250.jpg",
+						            "clickUrl": "http://tuan.qunar.com/static/topics/hotel/qixi/index.html",
+						            "enable": true,
+						            "sort": 1,
+						            "updateTime": 1443496880000
+						        },
+						        {
+						            "id": 14,
+						            "mainTitle": "",
+						            "subTitle": "",
+						            "simgUrl": "http://spimg.qunarzz.com/gongyu/20150721/clzs2.jpg",
+						            "imgUrl": "",
+						            "bimgUrl": "http://spimg.qunarzz.com/gongyu/20150721/clzsg.jpg",
+						            "clickUrl": "http://tuan.qunar.com/static/topics/hotel/chailvzhusu/index.html?from=gysy",
+						            "enable": true,
+						            "sort": 2,
+						            "updateTime": 1442471883000
+						        }
+						    ]
+						} ;
+			var first = template.compile(tpl.otherPage),
+                html = render({data : data[0]});
+            $('.step-1').append(html);
+
 		},
 
 		bind : function() {
-
+			var _this = this;
 		    // 初始化swiper
 		    var swiper = new Swiper('.swiper-container', {
 		      	direction: 'vertical',
 		      	loop: false,
-		      	onTouchMove: function(swiper){
-			    	alert('haha')
+		      	onSlideChangeStart: function(swiper){
+			    	_this.getList();
 			    }
 		    });
 
