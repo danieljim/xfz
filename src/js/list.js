12a9a7dc0c02ed5bf07d7687606be06ed080e083
dojay <wdojay@gmail.com>
2016-01-04 11:58:39 +0800

    data

--- src/js/list.js
+++ src/js/list.js
@@ -1035,11 +1035,11 @@ $(document).ready(function() {
 			var _this = this;
 			var listData = data;
 			var listArr1 = [data.data[0],data.data[1],data.data[2],data.data[3],data.data[4],data.data[5]];
-			var listArr2 = [data.data[6],data.data[7],data.data[8],data.data[9]];
-			var listArr3 = [data.data[12],data.data[13],data.data[14]];
-			var listArr4 = [data.data[18],data.data[19]];
-			var listArr5 = [data.data[24]];
-			var listArr6 = [data.data[30]];
+			var listArr2 = [data.data[6],data.data[7],data.data[8],data.data[9],data.data[10],data.data[11],,data.data[12],,data.data[13],,data.data[14]];
+			var listArr3 = [data.data[15],data.data[16],data.data[17],data.data[18],data.data[19],data.data[20],data.data[21],data.data[22],data.data[23]];
+			var listArr4 = [data.data[24],data.data[25],data.data[26],data.data[27],data.data[28],data.data[29],data.data[30],data.data[31],data.data[32]];
+			var listArr5 = [data.data[33],data.data[34],data.data[35],data.data[36],data.data[37],data.data[38],data.data[39],data.data[40],data.data[41]];
+			// var listArr6 = [data.data[42],data.data[43],data.data[44],data.data[45],data.data[46],data.data[47],data.data[48],data.data[49],data.data[50]];
 			
 			// 初始化swiper
 		    var swiper = new Swiper('.swiper-container', {
@@ -1107,11 +1107,11 @@ $(document).ready(function() {
 			var _this = this;
 			var listData = data;
 			var listArr1 = [data.data[0],data.data[1],data.data[2],data.data[3],data.data[4],data.data[5]];
-			var listArr2 = [data.data[6],data.data[7],data.data[8],data.data[9]];
-			var listArr3 = [data.data[12],data.data[13],data.data[14]];
-			var listArr4 = [data.data[18],data.data[19]];
-			var listArr5 = [data.data[24]];
-			var listArr6 = [data.data[30]];
+			var listArr2 = [data.data[6],data.data[7],data.data[8],data.data[9],data.data[10],data.data[11],,data.data[12],,data.data[13],,data.data[14]];
+			var listArr3 = [data.data[15],data.data[16],data.data[17],data.data[18],data.data[19],data.data[20],data.data[21],data.data[22],data.data[23]];
+			var listArr4 = [data.data[24],data.data[25],data.data[26],data.data[27],data.data[28],data.data[29],data.data[30],data.data[31],data.data[32]];
+			var listArr5 = [data.data[33],data.data[34],data.data[35],data.data[36],data.data[37],data.data[38],data.data[39],data.data[40],data.data[41]];
+			var listArr6 = [data.data[42],data.data[43],data.data[44],data.data[45],data.data[46],data.data[47],data.data[48],data.data[49],data.data[50]];
 			
 			// 初始化swiper
 		    var swiper = new Swiper('.swiper-container', {
